25384ecf611e954bc01d60c01721823915cd7532
Steven Kijooma <skijooma@andrew.cmu.edu>
2022-11-13 15:01:01 +0200

    feat: Project setup.

--- main.py
+++ main.py
@@ -11,7 +11,6 @@ import jwt
 # pylint: disable=import-error
 from flask import Flask, jsonify, request, abort
 
-
 JWT_SECRET = os.environ.get('JWT_SECRET', 'abc123abc1234')
 LOG_LEVEL = os.environ.get('LOG_LEVEL', 'INFO')
 
@@ -22,7 +21,8 @@ def _logger():
 
     RETURNS: log object
     '''
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    formatter = logging.Formatter(
+        '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
     log = logging.getLogger(__name__)
     log.setLevel(LOG_LEVEL)
@@ -35,25 +35,29 @@ def _logger():
 
 
 LOG = _logger()
-LOG.debug("Starting with log level: %s" % LOG_LEVEL )
+LOG.debug("Starting with log level: %s" % LOG_LEVEL)
 APP = Flask(__name__)
+
 
 def require_jwt(function):
     """
     Decorator to check valid jwt is present.
     """
+
     @functools.wraps(function)
     def decorated_function(*args, **kws):
-        if not 'Authorization' in request.headers:
+        if 'Authorization' not in request.headers:
             abort(401)
         data = request.headers['Authorization']
         token = str.replace(str(data), 'Bearer ', '')
+
         try:
             jwt.decode(token, JWT_SECRET, algorithms=['HS256'])
-        except: # pylint: disable=bare-except
+        except:  # pylint: disable=bare-except
             abort(401)
 
         return function(*args, **kws)
+
     return decorated_function
 
 
@@ -88,28 +92,31 @@ def decode_jwt():
     """
     Check user token and return non-secret data
     """
-    if not 'Authorization' in request.headers:
+    if 'Authorization' not in request.headers:
         abort(401)
     data = request.headers['Authorization']
     token = str.replace(str(data), 'Bearer ', '')
+
     try:
         data = jwt.decode(token, JWT_SECRET, algorithms=['HS256'])
-    except: # pylint: disable=bare-except
+    except:  # pylint: disable=bare-except
         abort(401)
 
+    response = {
+        'email': data['email'], 'exp': data['exp'], 'nbf': data['nbf']
+    }
 
-    response = {'email': data['email'],
-                'exp': data['exp'],
-                'nbf': data['nbf'] }
     return jsonify(**response)
 
 
 def _get_jwt(user_data):
     exp_time = datetime.datetime.utcnow() + datetime.timedelta(weeks=2)
-    payload = {'exp': exp_time,
-               'nbf': datetime.datetime.utcnow(),
-               'email': user_data['email']}
+    payload = {
+        'exp': exp_time, 'nbf': datetime.datetime.utcnow(),
+        'email': user_data['email']
+    }
     return jwt.encode(payload, JWT_SECRET, algorithm='HS256')
+
 
 if __name__ == '__main__':
     APP.run(host='127.0.0.1', port=8080, debug=True)
